62ae21a86fc5ed75f92e1d3e5a12ecab1a8ba8a0
Pringading <jess.marcell@gmail.com>
2025-02-03 15:55:32 +0000

    implement and test access_csv to return data as a list of dictionaries.

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,2 +1,14 @@
-def access_csv(path: str) -> dict:
-    return {}
+import boto3
+from io import StringIO
+from csv import DictReader
+
+def access_csv(bucket: str, key: str) -> str:
+    client = boto3.client('s3')
+    s3_object = client.get_object(
+        Bucket = bucket,
+        Key = key,
+    )
+    body = s3_object['Body'].read()
+    buffer = StringIO(body.decode('utf-8'))
+    reader = DictReader(buffer)
+    return list(reader)
