24cf98d7a79ba58b29d1f3eb917a59aa348dc56c
Pringading <jess.marcell@gmail.com>
2025-02-04 10:40:26 +0000

    implement and test obfuscate fields returns list of dictionaries.

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -3,28 +3,14 @@ from io import StringIO
 from csv import DictReader
 
 
-def access_csv(bucket: str, key: str) -> list[dict]:
-    """Function retrieves data from csv file in S3 bucket.
-
-    Args:
-        bucket(str): name of s3 bucket
-        key(str): the key where the csv file is located
-
-    Returns: data from the csv file as a list of dicitonaries."""
-
-    client = boto3.client('s3')
-    s3_object = client.get_object(
-        Bucket=bucket,
-        Key=key,
-    )
-    body = s3_object['Body'].read()
-    buffer = StringIO(body.decode('utf-8'))
+def object_body_to_list(body: str) -> list[dict]:
+    buffer = StringIO(body)
     reader = DictReader(buffer)
     return list(reader)
 
 
 def obfuscate_fields(rows: list[dict], fields: list[str]) -> list[dict]:
-    pass
+    return rows
 
 
 def list_to_csv_streaming_object():
--- test/test_csv_utils.py
+++ test/test_csv_utils.py
@@ -1,30 +1,66 @@
 import pytest
-from src.utils.csv_utils import access_csv
+from src.obfuscator import get_s3_object
+from csv import DictReader
+from src.utils.csv_utils import (
+    object_body_to_list,
+    obfuscate_fields,
+)
 
 
-class TestAccessS3CSV:
-    """Tests for access_csv function within src/obfuscator.py"""
-
-    @pytest.mark.it('Access csv returns list')
+class TestObjectBodyToList:
+    @pytest.mark.it('Returns list')
     def test_returns_list(self, mock_s3_bucket):
         test_bucket = 'test-bucket'
         test_key = 'students.csv'
-        result = access_csv(test_bucket, test_key)
+        test_object = get_s3_object(test_bucket, test_key)
+        result = object_body_to_list(test_object)
         assert isinstance(result, list)
 
-    @pytest.mark.it('Access csv returns list of dictionaries')
-    def test_returns_list_of_dictionaries(self, mock_s3_bucket):
+    @pytest.mark.it('Returns expected data')
+    def test_returns_expected_data(self, mock_s3_bucket):
         test_bucket = 'test-bucket'
         test_key = 'students.csv'
-        result = access_csv(test_bucket, test_key)
-        for item in result:
-            assert isinstance(item, dict)
+        test_object = get_s3_object(test_bucket, test_key)
+        result = object_body_to_list(test_object)
+        print(result)
+        with open("test/test_data/students.csv") as c:
+            expected = DictReader(c)
+            assert list(expected) == result
 
-    @pytest.mark.it('Access csv returns expected values')
-    def test_returns_expected_values(self, mock_s3_bucket):
-        test_bucket = 'test-bucket'
-        test_key = 'students.csv'
-        expected_names = ['Person 1', 'Person 2', 'Person 3']
-        result = access_csv(test_bucket, test_key)
-        print([row['name'] for row in result])
-        assert [row['name'] for row in result] == expected_names
+
+class TestObfuscateFields:
+    @pytest.mark.it('Returns list of dictioaries')
+    def test_returns_list_of_dicts(self):
+        test_list = [
+            {"name": "name 1"},
+            {"name": "name 2"}
+        ]
+        test_fields = ["name"]
+        result = obfuscate_fields(test_list, test_fields)
+        assert isinstance(result, list)
+        for row in result:
+            assert isinstance(row, dict)
+
+    @pytest.mark.skip
+    @pytest.mark.it('Obfuscates dictionaries with one field')
+    def test_obfuscate_1_field(self):
+        test_list = [
+            {"name": "name 1"},
+            {"name": "name 2"}
+        ]
+        test_fields = ["name"]
+        obfuscate_fields(test_list, test_fields)
+
+    @pytest.mark.skip
+    @pytest.mark.it('Obfuscates dictionaries with multiple fields')
+    def test_obfuscates_multiple_fields(self):
+        test_list = [
+            {"name": "name 1", "email": "1@email.com", "message": "hello"},
+            {"name": "name 2", "email": "2@email.com", "message": "world"},
+        ]
+        test_fields = ["name", "email"]
+        obfuscate_fields(test_list, test_fields)
+
+
+class TestListToCSVStreamingObject:
+    pass
