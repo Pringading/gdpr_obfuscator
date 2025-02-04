c9dfa9921c8581ae6711592780c085fc95adbab8
Pringading <jess.marcell@gmail.com>
2025-02-04 09:40:01 +0000

    test and implement get bucket and key from string function.

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -3,7 +3,12 @@ def obfuscator(json_str: str) -> None:
 
 
 def get_bucket_and_key_from_string(filename: str) -> tuple[str]:
-    pass
+    for i in range(5, len(filename)):
+        if filename[i] == "/":
+            break
+    bucket = filename[5: i]
+    key = filename[i + 1:]
+    return bucket, key
 
 
 def access_s3_object(bucket: str, key: str) -> str:
--- test/test_obfuscator.py
+++ test/test_obfuscator.py
@@ -1,9 +1,19 @@
+import pytest
+from src.obfuscator import get_bucket_and_key_from_string
+
+
 class TestObfuscator:
     pass
 
 
 class TestGetBucketAndKeyFromString:
-    pass
+    @pytest.mark.it('Obtains bucket and key name from s3 address.')
+    def test_returns_bucket_name(self):
+        test_file = "s3://my_ingestion_bucket/new_data/file1.csv"
+        expected_bucket = "my_ingestion_bucket"
+        expected_key = "new_data/file1.csv"
+        result = get_bucket_and_key_from_string(test_file)
+        assert result == (expected_bucket, expected_key)
 
 
 class TestAccessS3Object:
